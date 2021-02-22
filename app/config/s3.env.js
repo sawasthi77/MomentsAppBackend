c295a2dad309fb02518be46e229ae2725c21fa13
sawasthi77 <55201878+sawasthi77@users.noreply.github.com>
2021-02-22 14:26:55 +0530

    Update s3.env.js
    cannot add access key to public account

--- app/config/s3.env.js
+++ app/config/s3.env.js
@@ -1,6 +1,6 @@
 const env = {
-	AWS_ACCESS_KEY: 'AKIA35C64FBZJUZPTDIQ', // change to yours
-	AWS_SECRET_ACCESS_KEY: '77qPcocrCb1Sr6ff5QLubmdEbSbXiwwYwZfFCEon', // change to yours
+	AWS_ACCESS_KEY: '', // change to yours
+	AWS_SECRET_ACCESS_KEY: '', // change to yours
 	REGION : 'ap-south-1', // change to yours
 	Bucket: 'storemoments' // change to yours
 };
